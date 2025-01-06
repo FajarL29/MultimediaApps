93e167ed022e864c8575351f91de4d02948f12bf
fatimahazhr <fatimahazzahro20@gmail.com>
2025-01-06 14:47:46 +0700

    Perubahan Tipe Data
    Revisi Tipe Data & Variable

--- lib/core/service/read_heartrate2.dart
+++ lib/core/service/read_heartrate2.dart
@@ -3,7 +3,7 @@ import 'package:flutter_libserialport/flutter_libserialport.dart';
 
 class HeartRateService2 {
   final SerialPort _port =
-      SerialPort('/dev/ttyAMA10'); // Replace with your port
+      SerialPort('COM13'); // Replace with your port
   late SerialPortReader _reader;
   final StreamController<int> _heartRateController =
       StreamController<int>.broadcast();
@@ -11,10 +11,10 @@ class HeartRateService2 {
       StreamController<int>.broadcast();
   final StreamController<double> _bodyTempController =
       StreamController<double>.broadcast();
-  final StreamController<double> _sbpController =
-      StreamController<double>.broadcast();
-  final StreamController<double> _dbpController =
-      StreamController<double>.broadcast();
+  final StreamController<int> _sbpController =
+      StreamController<int>.broadcast();
+  final StreamController<int> _dbpController =
+      StreamController<int>.broadcast();
   final StreamController<int> _rrController = StreamController<int>.broadcast();
   String rawData = '';
 
@@ -24,8 +24,8 @@ class HeartRateService2 {
   Stream<int> get heartRateStream => _heartRateController.stream;
   Stream<int> get spO2RateStream => _spO2Controller.stream;
   Stream<double> get bodyTempStream => _bodyTempController.stream;
-  Stream<double> get sbpRateStream => _sbpController.stream;
-  Stream<double> get dbpRateStream => _dbpController.stream;
+  Stream<int> get sbpRateStream => _sbpController.stream;
+  Stream<int> get dbpRateStream => _dbpController.stream;
   Stream<int> get respRateStream => _rrController.stream;
 
   void startListening() {
@@ -86,7 +86,7 @@ class HeartRateService2 {
           }
           break;
         case 'ESBP':
-          double? esbp = double.tryParse(line[1]);
+          int? esbp = int.tryParse(line[1]);
           if (esbp != null) {
             _sbpController.add(esbp); // EspO2 heart rate
             print("ESBP Stream emitted: $esbp");
@@ -95,7 +95,7 @@ class HeartRateService2 {
           }
           break;
         case 'EDBP':
-          double? edbp = double.tryParse(line[1]);
+          int? edbp = int.tryParse(line[1]);
           if (edbp != null) {
             _dbpController.add(edbp); // EspO2 heart rate
             print("EDBP Stream emitted: $edbp");
