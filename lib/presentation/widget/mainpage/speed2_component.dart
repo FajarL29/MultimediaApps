cf3244886e0f2c1b7165b6b943e213b9fedae4d5
FajarL29 <fajarfirmanfirdaus71@gmail.com>
2025-06-24 16:36:34 +0700

    Merge voice command

--- lib/presentation/widget/mainpage/speed2_component.dart
+++ lib/presentation/widget/mainpage/speed2_component.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:multimedia_apps/core/constant/app_styles.dart';
 
@@ -63,69 +62,3 @@ class _SpeedDashboardState extends State<SpeedDashboard> {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:multimedia_apps/core/constant/app_styles.dart';
-
-class SpeedDashboard extends StatefulWidget {
-  const SpeedDashboard({super.key});
-
-  @override
-  State<SpeedDashboard> createState() => _SpeedDashboardState();
-}
-
-class _SpeedDashboardState extends State<SpeedDashboard> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16), // Padding ditambahkan
-                decoration: BoxDecoration(
-                  color: Color(0xFF334EAC).withOpacity(0.75),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/yaris1.webp'),
-    
-                    scale: 0.2,
-                    alignment: Alignment.center, // Gambar tetap di tengah
-                  ),
-                ),
-                child: Align(
-                  alignment: Alignment.topLeft, // Teks rata kiri
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min, 
-                    crossAxisAlignment: CrossAxisAlignment.start, // Pastikan teks mulai dari kiri
-                    children: [
-                      Text(
-                        "TOYOTA",
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      //SizedBox(height: 8), // Jarak antara dua teks
-                      Text(
-                        "Yaris Cross",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
->>>>>>> manual-book
--- lib/presentation/widget/mainpage/weather_component.dart
+++ lib/presentation/widget/mainpage/weather_component.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:multimedia_apps/core/constant/app_styles.dart';
 import 'package:multimedia_apps/presentation/widget/mainpage/date_time.dart';
@@ -61,67 +60,3 @@ class _WeatherComponentState extends State<WeatherComponent> {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:multimedia_apps/core/constant/app_styles.dart';
-import 'package:multimedia_apps/presentation/widget/mainpage/date_time.dart';
-
-class WeatherComponent extends StatefulWidget {
-  const WeatherComponent({super.key});
-
-  @override
-  State<WeatherComponent> createState() => _WeatherComponentState();
-}
-
-class _WeatherComponentState extends State<WeatherComponent> {
-  @override
-  Widget build(BuildContext context) {
-    String location = "Jakarta, Indonesia";
-    double temperature = 34;
-    String cloudyStatus = "Cloudy";
-    String rainyStatus = "Rainy";
-    String sunnyStatus = "Sunny";
-    bool isCloudy = temperature >= 28 && temperature <= 32;
-    bool isRainy = temperature < 28;
-
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          image: AssetImage(isCloudy
-              ? 'assets/images/weather_cloudy.png'
-              : isRainy
-                  ? "assets/images/weather_rainy.png"
-                  : "assets/images/weather_sunny.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const DateTimeWidget(), // Pastikan ini ada di dalam child
-            Text(location,
-                style: TextStyles.f20(context)
-                    .copyWith(color: AppStaticColors.white, fontSize: 20)),
-            Text("$temperature °",
-                style: TextStyles.f20(context)
-                    .copyWith(color: AppStaticColors.white, fontSize: 42)),
-            Text(
-                isCloudy
-                    ? cloudyStatus
-                    : isRainy
-                        ? rainyStatus
-                        : sunnyStatus,
-                style: TextStyles.f20(context)
-                    .copyWith(color: AppStaticColors.white, fontSize: 20)),
-          ],
-        ),
-      ),
-    );
-  }
-}
->>>>>>> manual-book
